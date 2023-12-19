b1bf5f4919253e3fa9aae0039866b7ff5e52ffc2
maaxizin25 <maaxipb25@gmail.com>
2023-12-19 14:08:17 -0300

    fix: test-one

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import "express-async-errors";
 import express, { Application, json } from "express";
 import { appRoutes } from "./routes/app/app.routes";
 import { handleErrors } from "./errors";
-import { CronJob } from "cron";
-import { api } from "./services/axios/api";
 
 export const app: Application = express();
 
@@ -12,21 +10,6 @@ export const list: { id: string | null; name: string | null } = {
   name: null,
 };
 export const numbers: string[] = [];
-
-new CronJob(
-  "00 21 * * * ",
-  async function () {
-    await api.delete(`list/${list.id}`);
-    list.id = null;
-    list.name = null;
-    numbers.length = 0;
-    console.log("Executei a deleção da lista as 21h");
-  },
-  null,
-  true,
-  "America/Sao_Paulo"
-);
-
 app.use(json());
 
 app.use("/webhook-ligue-lead", appRoutes);
